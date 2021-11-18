9eb61a28a96ff31b0617e1b233540bbf0ba6a1e7
Abdullah Metevski <abimet03@gmail.com>
2021-11-18 20:46:53 +0200

    Lab & Exercises

---
+++ SoftUni-CSharp-OOP/C# OOP Encapsulation Lab/02.Salary Increase/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lasName, int age,decimal solary)
+        {
+            FirstName = firstName;
+            LastName = lasName;
+            Age = age;
+            Solary = solary;
+        }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+        public decimal Solary { get; private set; }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age > 30)
+            {
+                this.Solary += this.Solary * percentage / 100;
+            }
+            else
+            {
+                this.Solary += this.Solary * percentage / 200;
+            }
+
+
+        }
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Solary:f2} leva.";
+        }
+    }
+}
